ef0d5d64094d6b6fb419b933af3f22708849fabc
Andrey <kotlyar.andrey03@mail.ru>
2024-11-29 21:30:32 +0300

    Стили кастомного сообщения

--- src/app.js
+++ src/app.js
@@ -1,2 +1,4 @@
 import './styles.css'
+import { CustomMessage } from './modules/customMessage.module'
 
+const message = new CustomMessage();
--- src/modules/customMessage.module.js
+++ src/modules/customMessage.module.js
@@ -1,3 +1,4 @@
+import '../styles.css';
 import { Module } from "../core/module";
 
 export class CustomMessage extends Module {
@@ -5,28 +6,26 @@ export class CustomMessage extends Module {
         super(1, 'fg'); //Не знаю что тут нужно, без этого не работает
 
         this.div = document.createElement('div');
-        this.div.style.border = '1px solid black'
-        this.div.style.width = '400px';
-        this.div.style.height = '200px';
-        this.div.style.display = 'none';
+        this.div.className = 'customMessage';
 
         this.span = document.createElement('span');
-        this.span.textContent = 'Hello World';
+        this.span.textContent = 'Живи, кайфуй, гуляй, играй, упал, вставай, наглей, ругай, чужих роняй, своих спасай, пельмени, суп, картошка, чай.';
+        this.span.style.color = 'white';
+        this.span.style.marginLeft = '5px'
+
+        this.h2 = document.createElement('h2');
+        this.h2.textContent = 'P.s Jason Statham';
 
         this.img = document.createElement('img');
+        this.img.src = 'src/assets/Jeison.png';
+        this.img.className = 'imgCustomMessage';
 
-        this.button = document.createElement('button');
-        this.button.style.width = '150px';
-        this.button.style.height = '50px';
-        this.button.textContent = 'Открыть сообщение';
-
+        this.span.append(this.h2);
         this.div.append(this.span);
+        this.div.append(this.img);
         document.body.append(this.div);
         document.body.append(this.button);
 
-        this.button.addEventListener('click', event => {
-            this.trigger();
-        });
     }
 
     trigger() {
@@ -35,6 +34,6 @@ export class CustomMessage extends Module {
         setTimeout(() => {
             this.div.style.display = 'none';
             this.button.style.display = 'block';
-        }, 3000);
+        }, 13000);
     };
 }
