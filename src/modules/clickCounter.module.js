d9ea6e4bc0342adb808b4a877d2c1b0a8b31aa8a
Egor-Cherepanov <105498881+Egor-Cherepanov@users.noreply.github.com>
2024-11-30 19:35:33 +0300

    add clickCounter

---
+++ src/modules/clickCounter.module.js
@@ -0,0 +1,40 @@
+import "../styles.css";
+import { Module } from "../core/module";
+
+export class clickCounter extends Module {
+  constructor(props) {
+    super("", "Считать клики (за 3 секунды)");
+
+    this.state = {
+      counter: 0,
+      time: 3,
+    };
+
+    this.div = document.createElement("div");
+    this.div.className = "clickCounter";
+
+    this.h1 = document.createElement("h1");
+    this.h1.textContent = `Количество кликов: ${this.state.counter}`;
+
+    this.timer = document.createElement("h2");
+    this.timer.textContent = `Количество кликов: ${this.state.time}`;
+    setInterval(() => {
+      this.timer.textContent = `Количество кликов: ${--this.state.time}`;
+    }, 1000);
+
+    document.body.addEventListener("click", () => {
+      this.h1.textContent = `Количество кликов: ${++this.state.counter}`;
+    });
+
+    this.div.append(this.h1);
+    this.div.append(this.timer);
+    document.body.append(this.div);
+  }
+
+  trigger() {
+    this.div.style.display = "block";
+    setTimeout(() => {
+      this.div.style.display = "none";
+    }, 3000);
+  }
+}
