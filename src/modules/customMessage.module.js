58056e4c46a04bd851d87d88ad1e2e9d4cb561a1
Andrey <kotlyar.andrey03@mail.ru>
2024-11-30 14:24:32 +0300

    Меню готово

--- src/modules/customMessage.module.js
+++ src/modules/customMessage.module.js
@@ -2,14 +2,14 @@ import { Module } from "../core/module";
 
 export class CustomMessage extends Module {
     constructor(props) {
-        super(1, 'Кастомное сообщение'); //Не знаю что тут нужно, без этого не работает
+        super(1, 'Кастомное сообщение'); 
 
         this.div = document.createElement('div');
         this.div.className = 'customMessage';
 
         this.span = document.createElement('span');
         this.span.textContent = 'Живи, кайфуй, гуляй, играй, упал, вставай, наглей, ругай, чужих роняй, своих спасай, пельмени, суп, картошка, чай.';
-        this.span.style.color = 'white';
+        this.span.style.color = 'black';
         this.span.style.marginLeft = '5px'
 
         this.h2 = document.createElement('h2');
@@ -23,8 +23,6 @@ export class CustomMessage extends Module {
         this.div.append(this.span);
         this.div.append(this.img);
         document.body.append(this.div);
-        document.body.append(this.button);
-
     }
 
     trigger() {
