2370f00daca3f09ae8efd90b7086f419e48d64f8
Mariya <quokka.mariya@gmail.com>
2024-11-30 16:49:52 +0300

    Сделала модуль фигур лучше

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,24 +1,55 @@
-import {Module} from '../core/module'
-import {random} from '../utils'
+import { Module } from '../core/module'
+import { random } from '../utils'
 
 export class ShapeModule extends Module {
     constructor(props) {
         super(3, 'Создать фигуру');
     }
-
+    
     trigger() {
-        this.shape = document.createElement('div');
-        this.size = random(50, 150);
-        this.color = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
-        this.x = random(0, window.innerWidth - this.size);
-        this.y = random(0, window.innerHeight - this.size);
-        this.shape.style.width = `${this.size}px`;
-        this.shape.style.height = `${this.size}px`;
-        this.shape.style.backgroundColor = this.color;
-        this.shape.style.position = 'absolute';
-        this.shape.style.left = `${this.x}px`;
-        this.shape.style.top = `${this.y}px`;
-        this.shape.style.borderRadius = Math.random() < 0.5 ? '50%' : '0';
-        document.body.append(this.shape);
+        this.createRandomShape();
+        setInterval(() => {
+            this.createRandomShape();
+            this.removeShape();
+        }, 2000);
     }
+
+    createRandomShape() {
+        const shapeType = random(0, 2); 
+        const size = random(20, 100); 
+        const height = random(20, 100); 
+        const color = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`; 
+        const x = random(0, window.innerWidth - size);
+        const y = random(0, window.innerHeight - size); 
+        
+        const shape = document.createElement('div');
+        shape.className = 'shape';
+        shape.style.position = 'absolute';
+        shape.style.left = `${x}px`;
+        shape.style.top = `${y}px`;
+        
+        if (shapeType === 0) { 
+            shape.style.width = `${size}px`;
+            shape.style.height = `${height}px`;
+            shape.style.backgroundColor = color;
+        } else if (shapeType === 1) { 
+            shape.style.width = `${size}px`;
+            shape.style.height = `${height}px`;
+            shape.style.borderRadius = '50%';
+            shape.style.backgroundColor = color;
+        } else if (shapeType === 2) { 
+            shape.style.width = '0';
+            shape.style.height = '0';
+            shape.style.borderLeft = `${size / 2}px solid transparent`;
+            shape.style.borderRight = `${size / 2}px solid transparent`;
+            shape.style.borderBottom = `${size}px solid ${color}`;
+        }
+    
+        document.body.append(shape); 
+    }    
+    
+    removeShape() {
+        document.querySelector('.shape').remove();
+    }
+          
 }
